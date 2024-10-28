816e21b9039eb882fc40f729140ed816e818534c
shinku1 <z1045367512@163.com>
2024-10-28 22:19:04 +0800

    proj1 ec 第四次提交

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -12,19 +12,24 @@ public class TestArrayDequeEC {
         StudentArrayDeque<Integer> buggyDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> correctDeque = new ArrayDequeSolution<>();
 
+        String operation = "";
+
         for (int i = 0; i < 100; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
             if (numberBetweenZeroAndOne > 0.5) {
                 buggyDeque.addLast(i);
                 correctDeque.addLast(i);
+                operation = "addLast(" + i + ")";
             } else {
                 if (!correctDeque.isEmpty()) {
                     Integer buggyLast = buggyDeque.removeLast();
                     Integer correctLast = correctDeque.removeLast();
-//                    assertEquals("\n"+ lastOperation + "\n" + thisOperation + "\n"
+//                    assertEquals(lastOperation + "\n" + thisOperation + "\n"
 //                            + "removeLast()\n", correctLast, buggyLast);
-                    assertEquals("removeLast()\n", correctLast, buggyLast);
+//                    assertEquals("removeLast()\n", correctLast, buggyLast);
+                    assertEquals(operation + "removeLast()\n", correctLast, buggyLast);
+
                 }
             }
         }
