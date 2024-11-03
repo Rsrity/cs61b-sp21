c5cdf0619bc6a416620ebb48df2481f0faaa9c3e
shinku1 <z1045367512@163.com>
2024-11-03 19:50:36 +0800

    lab7,第一次提交

---
+++ lab7/bstmap/BSTMap.java
@@ -0,0 +1,132 @@
+package bstmap;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+
+    private BSTNode root;                  // BST的根节点
+
+    private class BSTNode {
+        private K key;                     // 键
+        private V value;                   // 值
+        private BSTNode left, right;       // 左右子树
+        private int N;                     // 以此节点为根的子树的节点个数
+
+        BSTNode(K key, V value, int N) {
+            this.key = key;
+            this.value = value;
+            this.N = N;
+        }
+    }
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        if (root == null) {
+            return false;
+        }
+        BSTNode current = root;
+        while (current != null) {
+            if (key.equals(current.key)) {
+                return true;
+            } else if (key.compareTo(current.key) < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(K key) {
+        if (root == null) {
+            return null;
+        }
+        BSTNode current = root;
+        while (current != null) {
+            if (key.equals(current.key)) {
+                return current.value;
+            } else if (key.compareTo(current.key) < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return size(root);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private BSTNode put(BSTNode x, K key, V value){
+        if (x == null) {
+            return new BSTNode(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.value = value;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    private int size(BSTNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.N;
+    }
+
+
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    private void printInOrder(BSTNode node) {
+        if (node == null) {
+            return;
+        }
+        printInOrder(node.left);
+        System.out.print(node.key + " ");
+        printInOrder(node.right);
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+}
